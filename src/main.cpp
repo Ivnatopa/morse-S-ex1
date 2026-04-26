3a061857a0e2eee81a1298916bcb9b69f80e4371
ivnatopa <ivanatanasov.plovdiv@gmail.com>
2026-04-26 22:29:20 +0300

    opit1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_PIN 2 // Вграденият светодиод на ESP32 DevKit V1
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Конфигуриране на пина като изход
+  pinMode(LED_PIN, OUTPUT);
+  
+  // Инициализация на серийния монитор според platformio.ini
+  Serial.begin(115200);
+  
+  // Кратко закъснение за стабилизиране на порта
+  delay(1000); 
+  Serial.println("System Started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Цикъл за 3 кратки премигвания (Морзово 'S')
+  for(int i = 0; i < 3; i++) {
+    digitalWrite(LED_PIN, HIGH); // Включване на LED
+    delay(200);                  // Кратко задържане (200ms)
+    digitalWrite(LED_PIN, LOW);  // Изключване на LED
+    delay(200);                  // Кратка пауза (200ms)
+  }
+  
+  // Дълга пауза преди повторение на символа
+  delay(1500); 
 }
